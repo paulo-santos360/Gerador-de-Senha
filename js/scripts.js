644698dc567c23624c0c306046a9068be3348137
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-11 18:48:27 -0300

    Atualizar

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,50 @@ const getLetterUpperCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 };
 
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+    const symbols = "(){}=<>/,.!@#$%&*+^~";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+
+    let password = ""
+
+    const passwordLength = 10;
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol,
+    ]
+
+    for(i = 0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+            const randomValue = 
+            generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+        })
+    }
+    password = password.slice(0, passwordLength);
+
+    //console.log(password);
+};
+
+//console.log(getLetterUpperCase())
+
 //Eventos
 generatePasswordButton.addEventListener("click", () => {
     /* alert("ola"); */
+    generatePassword(
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    )
 });
